4e4c7484b0fcbe5be7d7ebeb227ce9319c8b0d91
LayanOliveira <layan3210@gmail.com>
2022-07-10 21:51:48 -0300

    Define interface exercise

---
+++ Type-and-JavaScript/Ex01-define-interface/Solution01.js
@@ -0,0 +1,20 @@
+"use strict";
+//Exercise:
+;
+const users = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+function logPerson(user) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+console.log('Users:');
+users.forEach(logPerson);
